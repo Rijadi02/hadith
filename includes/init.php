cf665ac056d74d7449b14da3cdbc42a9d3a44f1c
Rijadi02 <adiveni0@gmail.com>
2020-08-11 10:33:26 +0200

    init

--- includes/init.php
+++ includes/init.php
@@ -4,12 +4,12 @@ require_once "database.php";
 require_once "functions.php";
 require_once "db_object.php";
 require_once "page.php";
-require_once "email_list.php";
-require_once "email.php";
 
 require_once "librat.php";
 require_once "kapitujt.php";
 require_once "hadithet.php";
+require_once "email_list.php";
+require_once "email.php";
 
 
 include "head.php";
