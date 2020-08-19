a21a862935903778fe8bf77c0ce5aa19521deca8
Rijadi02 <adiveni0@gmail.com>
2020-08-19 14:09:13 +0200

    libri

--- api/random.php
+++ api/random.php
@@ -3,4 +3,11 @@ header("Access-Control-Allow-Origin: *");
 
 require_once("../includes/init_head.php");
 
-echo json_encode(Hadithet::get_random());
+$hadith = Hadithet::get_random();
+
+$array = ["Hadithi" => $hadith->Hadithi,
+        "Transmetimi" => $hadith->Transmetimi,
+        "Libri" => $hadith->get_chapter()->NrKapitulli . ", " .$hadith->get_book()->Libri,
+        "Shkalla" => $hadith->Shkalla];
+
+echo json_encode($array);
