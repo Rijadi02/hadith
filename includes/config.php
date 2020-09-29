5f24efd2d314dae1fb4196d237952259dda6df4e
Rijadi02 <41744351+Rijadi02@users.noreply.github.com>
2020-09-29 18:39:25 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,9 +4,9 @@
 ob_start();
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'u321225794_rijadi02');
+define('DB_USER', 'u321225794_root');
 define('DB_PASS', 'Blender3&');
-define('DB_NAME', 'u321225794_hadith');
+define('DB_NAME', 'u321225794_hadiths');
 
 // define('DB_HOST', 'localhost');
 // define('DB_USER', 'root');
