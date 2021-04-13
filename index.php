85f65e7d5cbab4976f98ba9c8d935a3697b276ae
venismorina <venismorinax8@gmail.com>
2021-04-13 17:16:18 +0200

    logo

--- index.php
+++ index.php
@@ -23,15 +23,6 @@ if (isset($_POST['type'])) {
     }
 </script>
 
-<style>
-@media screen and (min-color-index:0) and(-webkit-min-device-pixel-ratio:0)
-{ @media {
-    .logo-safari{
-        height: 52px;
-        width: 48px;
-    }
-}}
-</style>
 
 
 
@@ -115,7 +106,7 @@ if (isset($_POST['type'])) {
                                 </div>
                                 <div class="row text-center">
                             
-                                            <img style="margin: 0 auto; width:48px" class="pb-5 logo-safari"  src="assets/img/mi.png"/>
+                                            <img style="margin: 0 auto; width:48px; object-fit: contain;" class="pb-5"  src="assets/img/mi.png"/>
                                             <!-- <p class="page-header-text">Muslimani ideal</p> -->
                             
                                     <p class="page-header-text pb-4 w-100">The ahadith that are shown on this page are taken from <a href="http://www.hadithet.com">hadithet.com </a> and <a href="http://www.sunnah.com">sunnah.com </a> </p>
